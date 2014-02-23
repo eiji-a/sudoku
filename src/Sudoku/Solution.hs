f07dc6be7b507940ebca64b2ae38dfc518a505a2
Eiji Akagi <akagi.eiji@gmail.com>
2014-02-22 19:34:23 -0600

    fixed test

--- src/Sudoku/Solution.hs
+++ src/Sudoku/Solution.hs
@@ -16,12 +16,39 @@ type Position = Int
 type Value    = Int
 type Solution = (Position, Value)
 
-posToXy :: Position -> (Int, Int)
-posToXy pos = (x, y)
+posToXy :: Position -> Maybe (Int, Int)
+posToXy pos
+  | pos < 0            = Nothing
+  | pos >= boardSize^2 = Nothing
+  | otherwise          = Just (x, y)
   where
     x = pos `mod` boardSize + 1
     y = pos `div` boardSize + 1
---    y = pos / boardSize + 1
 
-xyToPos :: (Int, Int) -> Position
-xyToPos (x,y) = (x - 1) + (y - 1) * boardSize
+xyToPos :: (Int, Int) -> Maybe Position
+xyToPos (x,y) 
+  | p < 0            = Nothing
+  | p >= boardSize^2 = Nothing
+  | otherwise        = Just p
+  where
+    p = (x - 1) + (y - 1) * boardSize
+
+{-
+solutionX :: Solution -> Int
+solutionX (p, v)
+  | xy == Nothing     = 0
+  | xy == Just (x, y) = x
+  where
+    xy = posToXy p
+
+solutionY :: Solution -> Int
+solutionY (p, v)
+  | xy == Nothing     = 0
+  | xy == Just (x, y) = y
+  where
+    xy = posToXy p
+
+solutionVal :: Solution -> Value
+solutionVal (p, v) = v
+-}
+
--- src/UnitTest/Sudoku/Solution.hs
+++ src/UnitTest/Sudoku/Solution.hs
@@ -17,12 +17,27 @@ main = defaultMain testSuite
 
 testSuite = hUnitTestToTests $ tests
 
-tests =  "Sudoku Solution" ~: test_value
-
-test_value = "value" ~:
-  test [
-    "pos 0" ~: posToXy 0 ~=? (1, 1)
-  , "pos 1" ~: posToXy 1 ~=? (2, 1)
+tests = "Sudoku Solution" ~: [
+    "posToXy" ~:
+    test [
+      "pos 0" ~: posToXy 0 ~=? Just (1, 1)
+    , "pos 1" ~: posToXy 1 ~=? Just (2, 1)
+    , "pos 40" ~: posToXy 40 ~=? Just (5, 5)
+    , "pos 80" ~: posToXy 80 ~=? Just (9, 9)
+    , "pos -1" ~: posToXy (-1) ~=? Nothing
+    , "pos -5" ~: posToXy (-5) ~=? Nothing
+    , "pos 81" ~: posToXy 81 ~=? Nothing
+    ]
+  , "xyToPos" ~:
+    test [
+      "xy 1,1" ~: xyToPos (1,1) ~=? Just 0
+    , "xy 2,1" ~: xyToPos (2,1) ~=? Just 1
+    , "xy 9,9" ~: xyToPos (9,9) ~=? Just 80
+    , "xy 0,0" ~: xyToPos (0,0) ~=? Nothing
+    , "xy -1,0" ~: xyToPos ((-1),0) ~=? Nothing
+    , "xy 10,9" ~: xyToPos (10,9) ~=? Nothing
+    , "xy 9,10" ~: xyToPos (9,10) ~=? Nothing
+    ]
   ]
 
 test_conversion = [
